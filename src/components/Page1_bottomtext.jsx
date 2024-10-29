8fee9c3df5c4c34de559b4a77cedea2fe63d8ef1
ripgamer <akashdeepkumar0001@gmail.com>
2024-10-29 21:29:49 +0530

    page1 completed

--- src/components/Page1_bottomtext.jsx
+++ src/components/Page1_bottomtext.jsx
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import { gsap } from 'gsap';
 
 export class Page1_bottomtext extends Component {
+  componentDidMount() {
+    this.animateImages();
+  }
+
+  animateImages() {
+    gsap.to(this.img1, {
+      duration: 5,
+      rotation: 360,
+      repeat: -1,
+      ease: 'linear',
+    });
+
+    gsap.to(this.img2, {
+      duration: 5,
+      rotation: 360,
+      repeat: -1,
+      ease: 'linear',
+    });
+  }
+
   render() {
     return (
-      <div className='text-white  w-full text-[2.25vh] text-left bottom-0 left-0 flex items-end justify-between '>
-        <div className='flex flex-col  '>
+      <div className='text-white w-full text-[2.25vh] text-left bottom-0 left-0 flex items-end justify-between'>
+        <div className='flex flex-col'>
           <h1 className='font-[anzo2]'>BRAND DESIGN | WEBSITE DESIGN</h1>
           <h1 className='font-[anzo3] leading-'>BESPOKE FREELANCE</h1>
         </div>
-        <div className='flex flex-col mt-[-10vh] '>
-          <img className=' mb-5' src="./src/assets/img/wix2.png" alt="Image 1" />
-          <img src="./src/assets/img/wix.png" alt="Image 2" />
+        <div className='flex flex-col mt-[-10vh]'>
+          <img
+            ref={(el) => (this.img1 = el)}
+            className='mb-5'
+            src='./src/assets/img/wix2.png'
+            alt='Image 1'
+          />
+          <img
+            ref={(el) => (this.img2 = el)}
+            src='./src/assets/img/wix.png'
+            alt='Image 2'
+          />
         </div>
       </div>
     );
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,27 +1,64 @@
-import React from 'react'
-import Tilttext from '../components/Tilttext'
-import Page1_bottomtext from '../components/Page1_bottomtext'
+import React, { useRef, useState } from 'react';
+import Tilttext from '../components/Tilttext';
+import Page1_bottomtext from '../components/Page1_bottomtext';
+import { gsap } from 'gsap';
+
 
 function Page1() {
-    return (
-        <div className='h-screen   w-full bg-white px-4 py-5 '>
+    
+    const tiltRef = useRef(null);
+    const [valX, setvalX] = useState(0);
+    const [valY, setvalY] = useState(0);
 
-            <div className='absolute  py-11 px-16 w-full'>
-                <img className='h-20' src="./src/assets/img/logo.png" alt="" />
-                <Tilttext/>
-                <Page1_bottomtext/>
+    const mouseMoving = (e) => {
+        const rect = tiltRef.current.getBoundingClientRect();
+        const centerX = rect.x + rect.width / 2;
+        const centerY = rect.y + rect.height / 2;
+
+        setvalX((e.clientX - centerX) / 20);
+        setvalY((centerY - e.clientY) / 20);
+
+        gsap.to(tiltRef.current, {
+            duration: 10,
+            rotateY: valX,
+            rotateX: valY,
+            ease: 'elastic',
+          });
+
+        tiltRef.current.style.transform = `rotateY(${valX}deg) rotateX(${valY}deg)`;
+    };
+   
+
+    return (
+        <div
+            onMouseMove={(e) => {
+                mouseMoving(e);
+            }}
+            className='h-screen w-full bg-white px-4 py-5'
+        >
+            <div id='page1-in' className='absolute py-11 px-16 w-full'>
+                <img className='h-20' src='./src/assets/img/logo.png' alt='' />
+                {/* <Tilttext/> */}
+
+                <div
+                    id='tiltdiv'
+                    ref={tiltRef}
+                    className='text-white text-left font-[anzo2] text-[4.2vw] mt-56'
+                    style={{ transform: 'rotateY(0deg) rotateX(0deg)' }}
+                >
+                    <h1 className='leading-[4vw]'>
+                        I AM<span className='text-black'> DARK MODE</span>™
+                    </h1>
+                    <h1 className='text-[8.5vw] leading-[7vw]'>DESIGNER</h1>
+                    <h1 className='leading-[4vw]'>TO HIRE</h1>
+                </div>
+
+                <Page1_bottomtext />
             </div>
 
-
-            <div className='h-full w-full shadow-black shadow-lg bg-[url(./src/assets/img/bg.png)]  bg-cover rounded-[40px] '></div>
-            
-
-            
-
-
-
+            <div className='h-full w-full shadow-black shadow-lg bg-[url(./src/assets/img/bg.png)] bg-cover rounded-[40px]'></div>
         </div>
-    )
+    );
 }
 
-export default Page1
+export default Page1;
