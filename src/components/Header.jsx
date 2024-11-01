aadb748b9f6d5a915cf2f9b5ab6c81edec8c191d
ripgamer <akashdeepkumar0001@gmail.com>
2024-11-01 11:22:35 +0530

    added linkdin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,13 @@ export class Header extends Component {
   render() {
     return (
       <div className='fixed w-full z-10 flex items-center justify-end py-16 p-16 gap-3'>
-        <button className='text-white hover:bg-gray-600 font-[anzo4] bg-black border-4 rounded-full px-10 py-2'>Hire me</button>
-        <i className="ri-menu-2-line font-bold text-2xl text-center hover:text-gray-600"></i>
-
-       
-
+       <a href='https://www.linkedin.com/in/aakashdeepkumar/'
+        className='text-white hover:bg-gray-600 font-[anzo4] bg-black border-4 rounded-full px-10 py-2'
+        target='_blank' rel='noopener noreferrer'
+      >
+        Hire me
+      </a>
+      <i className="ri-menu-2-line font-bold text-2xl text-center hover:text-gray-600"></i>
       </div>
     )
   }
